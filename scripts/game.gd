4855721e648708c1dda74bcf251ae9f65723138d
NileKnight0 <ammartube777@gmail.com>
2026-09-17 06:35:33 +0300

    added baisc player, basic jump, bascic spawning system, basic death, basic obstacles movin

---
+++ scripts/game.gd
@@ -0,0 +1,30 @@
+extends Node2D
+
+
+func _ready() -> void:
+	var obj = $reference_nodes/obstacles/obj1.duplicate()
+	#obj.position = $reference_nodes/obstacles/obj1.position
+	$moving_nodes/obstacles.add_child(obj)
+	
+	await get_tree().create_timer(3.0).timeout
+	#var obj2 = $reference_nodes/obstacles/obj1.duplicate()
+	#print($reference_nodes/obstacles/obj1.global_position, " $reference_nodes/obstacles/obj1.global_position")
+	#print(obj.global_position, " obj.global_position")
+	
+	
+	obj = $reference_nodes/obstacles/obj1.duplicate()
+	#obj.position = $reference_nodes/obstacles/obj1.position
+	$moving_nodes/obstacles.add_child(obj)
+	$reference_nodes/obstacles.get_children()
+
+func _process(delta: float) -> void:
+	#$moving_nodes.position.x -= 1
+	for i in $moving_nodes/obstacles.get_children():
+		i.position.x -= 1
+		#print($moving_nodes/obstacles.get_children())
+	
+	pass
+
+func _on_obstacle_body_entered(body: Node2D) -> void:
+	if body == $player:
+		print("dead")
