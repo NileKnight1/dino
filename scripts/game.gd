b17274e538f942cfe8bc4bc651e08bb277bc4f7a
NileKnight0 <ammartube777@gmail.com>
2026-09-17 07:02:57 +0300

    added double jump effect and crawl effect

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,25 +2,35 @@ extends Node2D
 
 
 func _ready() -> void:
-	var obj = $reference_nodes/obstacles/obj1.duplicate()
-	#obj.position = $reference_nodes/obstacles/obj1.position
+	#var obj = $reference_nodes/obstacles/obj1.duplicate()
+	##obj.position = $reference_nodes/obstacles/obj1.position
+	#$moving_nodes/obstacles.add_child(obj)
+	#
+	#await get_tree().create_timer(3.0).timeout
+	##var obj2 = $reference_nodes/obstacles/obj1.duplicate()
+	##print($reference_nodes/obstacles/obj1.global_position, " $reference_nodes/obstacles/obj1.global_position")
+	##print(obj.global_position, " obj.global_position")
+	#
+	#obj = $reference_nodes/obstacles/obj1.duplicate()
+	##obj.position = $reference_nodes/obstacles/obj1.position
+	#$moving_nodes/obstacles.add_child(obj)
+	#$reference_nodes/obstacles.get_children()
+	
+	spawn()
+	pass
+
+func spawn():
+	var temp = randi_range($reference_nodes/obstacles.get_child_count()-1, 0)
+	var obj = $reference_nodes/obstacles.get_child(temp).duplicate()
 	$moving_nodes/obstacles.add_child(obj)
 	
-	await get_tree().create_timer(3.0).timeout
-	#var obj2 = $reference_nodes/obstacles/obj1.duplicate()
-	#print($reference_nodes/obstacles/obj1.global_position, " $reference_nodes/obstacles/obj1.global_position")
-	#print(obj.global_position, " obj.global_position")
-	
-	
-	obj = $reference_nodes/obstacles/obj1.duplicate()
-	#obj.position = $reference_nodes/obstacles/obj1.position
-	$moving_nodes/obstacles.add_child(obj)
-	$reference_nodes/obstacles.get_children()
+	await get_tree().create_timer(2.5).timeout
+	spawn()
 
 func _process(delta: float) -> void:
 	#$moving_nodes.position.x -= 1
 	for i in $moving_nodes/obstacles.get_children():
-		i.position.x -= 1
+		i.position.x -= 240 * delta
 		#print($moving_nodes/obstacles.get_children())
 	
 	pass
--- scripts/playerx.gd
+++ scripts/playerx.gd
@@ -4,14 +4,33 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -500.0
 
+var jumping = 0
+var ducking = 0
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity()*1.5 * delta
 
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") && jumping == 0:
 		velocity.y = JUMP_VELOCITY
-
+		jumping += 1
+	
+	if is_on_floor():
+		jumping = 0
+	
+	if Input.is_action_pressed("duck") && is_on_floor():
+		print("duc")
+		ducking = 1
+		$normal.visible = 0
+		$duck.visible = 1
+		$col_normal.set_deferred("disabled", 1)
+		$col_duck.set_deferred("disabled", 0)
+	else:
+		ducking = 0
+		$normal.visible = 1
+		$duck.visible = 0
+		$col_normal.set_deferred("disabled", 0)
+		$col_duck.set_deferred("disabled", 1)
 	#var direction := Input.get_axis("ui_left", "ui_right")
 	#if direction:
 		#velocity.x = direction * SPEED
