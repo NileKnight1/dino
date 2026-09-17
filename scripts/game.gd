7a072e7c26e87a05a3c717c9dfc611f1c4c5c400
NileKnight0 <ammartube777@gmail.com>
2026-09-17 19:36:07 +0300

    added difficulty increasing feature, added safe time when rushing ends, adjusted rushing feature

--- scripts/game.gd
+++ scripts/game.gd
@@ -20,27 +20,35 @@ func _ready() -> void:
 	$player.game = self
 	spawn()
 	score_inc()
+	#difficulty()
+
+var def_speed
+var def_wait_time
 
 func spawn():
-	#spawn_obstacles()
-	#spawn_powers()
+	spawn_obstacles()
+	spawn_powers()
 	spawn_coins()
-
-
 
 func spawn_coins():
 	if stop_game: return
-	print("spawn coins")
+	#print("spawn coins")
 	var temp = randi_range($reference_nodes/coins.get_child_count()-1, 0)
 	var obj = $reference_nodes/coins.get_child(temp).duplicate()
 	$moving_nodes/coins.add_child(obj)
 	
-	await get_tree().create_timer(wait_time).timeout
+	await get_tree().create_timer(wait_time*1.9).timeout
 	spawn_coins()
 
 func spawn_powers():
 	#print("power spawned")
 	if stop_game: return
+	if stop_spawn:
+		await get_tree().create_timer(wait_time).timeout
+		spawn_obstacles()
+		return
+	if player_rushing: return
+	
 	var temp = randi_range($reference_nodes/powers.get_child_count()-1, 0)
 	var obj = $reference_nodes/powers.get_child(temp).duplicate()
 	$moving_nodes/powers.add_child(obj)
@@ -52,6 +60,10 @@ func spawn_powers():
 
 func spawn_obstacles():
 	if stop_game: return
+	if stop_spawn:
+		await get_tree().create_timer(wait_time).timeout
+		spawn_obstacles()
+		return
 	#print("obstacle spawned")
 	var temp = randi_range($reference_nodes/obstacles.get_child_count()-1, 0)
 	var obj = $reference_nodes/obstacles.get_child(temp).duplicate()
@@ -69,7 +81,6 @@ var score = 0
 var coins = 0
 
 func score_inc():
-	
 	await get_tree().create_timer(0.1).timeout
 	if stop_game: return
 	
@@ -78,6 +89,19 @@ func score_inc():
 
 func coin_add():
 	coins += 1
+
+func difficulty():
+	#if player_rushing:
+		#
+		#dif
+	#
+	await get_tree().create_timer(5).timeout
+	print("diff increased")
+	speed += 20
+	wait_time -= 0.2
+	
+	difficulty()
+	
 
 func _process(delta: float) -> void:
 	#$moving_nodes.position.x -= 1
@@ -109,6 +133,7 @@ func _on_obstacle_body_entered(body: Node2D) -> void:
 			return
 		
 		return
+		$CanvasLayer/red.visible = 1
 		stop_game = 1
 		$player.move = 0
 		$CanvasLayer/restart.visible = 1
@@ -116,6 +141,7 @@ func _on_obstacle_body_entered(body: Node2D) -> void:
 
 var player_shield = 0
 var player_rushing = 0
+var stop_spawn = 0
 
 func _on_sheild_taken(body: Node2D) -> void:
 	if body == $player:
@@ -125,13 +151,19 @@ func _on_rush_taken(body: Node2D) -> void:
 	if body == $player:
 		print("rush")
 		player_rushing = 1
+		def_speed = speed
+		def_wait_time = wait_time
+		
 		speed = 1000
 		wait_time= 0.5
 		$CanvasLayer/rush.text = "Rush: " + str(1) 
-		await get_tree().create_timer(3.0).timeout
+		await get_tree().create_timer(5.0).timeout
+		stop_spawn = 1
+		await get_tree().create_timer(1.0).timeout
 		$CanvasLayer/rush.text = "Rush: " + str(0)
-		speed = 240
-		wait_time= 2
+		stop_spawn = 0
+		speed  = def_speed
+		wait_time  = def_wait_time
 		player_rushing = 0
 		
 func _on_restart_pressed() -> void:
@@ -139,7 +171,5 @@ func _on_restart_pressed() -> void:
 
 #func _on_coin_collected(body: Node2D) -> void:
 	#$reference_nodes/coin.visible = 0
-
-
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print(body)
